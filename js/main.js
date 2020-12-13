7d60f789d0233074308ef940b0401058ae8b66d9
ASIM SAMI <eng.asimsami@gmail.com>
2020-12-13 15:10:43 +0300

    Add auto jump to target char & speed up the abyess

--- js/main.js
+++ js/main.js
@@ -2,28 +2,30 @@ let cant_jump = 0;
 
 // animation of char walking.
 var main_char_moving = "main_char_moving 300ms infinite";
+var target_char_moving = "target_char_moving 300ms infinite";
 $( document ).ready(()=>{
     $("#main-char").css({
         "animation": `${main_char_moving}`
     });
 
     $("#target-char").css({
-        "animation": `target_char_moving 300ms infinite`
+        "animation": `${target_char_moving}`
     });
 })
 
 
 
-var abyss_size = 0;
-// Random width of element hole.
+// var abyss_size = 0;
+// // Random width of element hole.
 // var abyss_size = setInterval (()=>{
 //     var abyssRandom = Math.random() * (20 - 8) + 8;
-//     $("#abyss").css({ "width": `${abyssRandom}%` })
+//     $("#abyss").css({ "width": `${20}%` })
 //     abyss_size = $("#abyss").css("width");
-//     console.log(abyss_size)
     
 // } , 5000);
 
+
+function CheckIfCharFaling(lessThan,MoreThan){
 
 // Check if characters droped in the abyss.End The Game.
 var endGame = setInterval (()=>{
@@ -34,13 +36,30 @@ var main_char = $("#main-char");
 var mainChar_top_distance = parseInt(main_char.css("top"));
 var abyss_top_distance  = parseInt(abyss.css("top"));
 var abyss_left_distance  = parseInt(abyss.css("left"));
+var target_char_jump_ones = 0;
+if(abyss_left_distance < 150 && target_char_jump_ones === 0){
+  
+        $("#target-char").css({
+            "animation": `target-char-jump 1000ms`})
+            target_char_jump_ones = 1;
+            setTimeout(()=>{
+        
+                $("#target-char").css({
+                    "animation": `${target_char_moving}`})
+                    target_char_jump_ones = 0;
+                }, 1000);
+        
+    
+    
+}
 
 
-if( abyss_left_distance < 20 && abyss_left_distance > 0  &&
+if( abyss_left_distance < lessThan && abyss_left_distance > MoreThan  &&
     mainChar_top_distance === abyss_top_distance){
        
        cant_jump =1;
        main_char.css({"animation": `lose 500ms`});
+       $("#target-char").css({"animation": ``});
        clearInterval(endGame)
 
        abyss.css({ "left": `${abyss_left_distance}px` })
@@ -49,11 +68,16 @@ if( abyss_left_distance < 20 && abyss_left_distance > 0  &&
        $("#ground").css("animation","none")
        setTimeout(() =>{
             alert("You Lose!!"); }, 1000
-            );
+            )
 
+
+ }
+},0.0001);
 }
 
-},1);
+
+    CheckIfCharFaling(20,0)
+
 
 
 
@@ -64,7 +88,7 @@ var main_char_jumpOnSpaceBar =
 $("body").on('keypress', (evt) => {
     if (evt.which == 32 && cant_jump === 0) {
         $("#main-char").css({
-            "animation": `jump 1600ms`
+            "animation": `main-char-jump 1600ms`
         })
         setTimeout(()=>{
 
@@ -87,7 +111,7 @@ var main_char_jumpOnClick =
 $("body").on('click', () => {
 
         if(cant_jump === 0){
-            $("#main-char").css({ "animation": `jump 1600ms` })
+            $("#main-char").css({ "animation": `main-char-jump 1600ms` })
             setTimeout(()=>{
     
                 $("#main-char").css({
@@ -104,27 +128,7 @@ $("body").on('click', () => {
 
 
 
-
-
-
           
-
-     
-// setInterval(()=>{
-    
-//     $("#target-char").addClass("jump");
-//     setTimeout(()=>{
-       
-//      $("#target-char").removeClass("jump"); 
-  
-// },1600);
-// },1600);
-
-
-
-
-
-
 
 
 
