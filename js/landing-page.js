a31151ba62ac03e4760e94e9133ae6b04aec75e8
ASIM SAMI <eng.asimsami@gmail.com>
2020-12-15 15:41:42 +0300

    add stage two of how to play

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,4 +1,5 @@
 $("#next-button").on("click",()=>{
-
-   
+    $("#how-to-play").text("")
+    $("#how-to-play").append("How To play:<br><br> Collect The stars to buy itemes and use it to damage the Imposter!.<br><br>")
+    $("#imges").css("background-image",'url("../img/img-ex-click-stars-itemes.jpg")')
 })
