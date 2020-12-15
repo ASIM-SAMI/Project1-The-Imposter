20cb5647129c71c6d42582941da4aedf1d89f611
ASIM SAMI <eng.asimsami@gmail.com>
2020-12-15 17:15:09 +0300

    more modification to landing page

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,5 +1,27 @@
+var next = 0;
+var p_before = "";
 $("#next-button").on("click",()=>{
-    $("#how-to-play").text("")
-    $("#how-to-play").append("How To play:<br><br> Collect The stars to buy itemes and use it to damage the Imposter!.<br><br>")
-    $("#imges").css("background-image",'url("../img/img-ex-click-stars-itemes.jpg")')
+  
+
+switch(next){
+   case 0:
+    pExplainHowTP("How To play:<br><br> Collect The stars to buy itemes and use it to damage the Imposter!.<br><br>",
+    'url("../img/img-ex-click-stars-itemes.jpg")')
+    break;
+    
+    case 1: 
+    pExplainHowTP("These items You can buy it by stars to damage the Imposter to catch him!!",
+    'url("../img/img-ex-item-damage.jpg")')
+}
+
+next += 1;
 })
+
+function pExplainHowTP(nextP, nextImg ){
+
+    p_before = $("#how-to-play").text()
+    $("#how-to-play").text("")
+    $("#how-to-play").append(nextP)
+    $("#imges").css("background-image",nextImg)
+
+}
