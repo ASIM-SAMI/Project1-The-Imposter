90fb47db53e342404692d1d4d27d48e4fd914f56
ASIM SAMI <eng.asimsami@gmail.com>
2020-12-15 23:01:48 +0300

    Many fixes & add bar KG drop (item2) & add win if u catch the target

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var cant_jump = 0;
 var kiled = 0;
 var double_jump =0;
-var total_stars = 0;
+var total_stars = 48;
 var damage = 0;
 var target_hp = 5;
 var item1 = 0;
+var item2 = 0;
+var item3 = 0;
 
 // animation of char walking on js load.
 var main_char_moving = "main_char_moving 300ms infinite";
@@ -27,25 +29,14 @@ $( document ).ready(()=>{
 })
 
 
-
-// var abyss_size = 0;
-// // Random width of element hole.
-// var abyss_size = setInterval (()=>{
-//     var abyssRandom = Math.random() * (20 - 8) + 8;
-//     $("#abyss").css({ "width": `${20}%` })
-//     abyss_size = $("#abyss").css("width");
-    
-// } , 5000);
-
-
-
 // Char Target sensor forword to check if there any abyss. Then jump.
 function target_sensorForJump(abyss_left_distance,target_char_jump_ones){
 
     var target_sensor_distanceLeftStart = parseInt($("#target_sensor_toJump").css("left"));
     var target_sensor_distanceLeftEnd = target_sensor_distanceLeftStart-100;
+    
 
-    if(abyss_left_distance <= target_sensor_distanceLeftStart &&
+ if(abyss_left_distance <= target_sensor_distanceLeftStart &&
     abyss_left_distance >= target_sensor_distanceLeftEnd &&
     target_char_jump_ones === 0){
   
@@ -64,18 +55,34 @@ function target_sensorForJump(abyss_left_distance,target_char_jump_ones){
 }
 }
 
-function targetDamage(target_left_distance,target_top_distance,
-    item1_left_distance,item1_top_distance,continer_size){
+function targetDamage(item,target_left_distance,target_top_distance,
+    item_left_distance,item_top_distance,continer_size,target_sensor_left_d){
 
-        if(target_left_distance === item1_left_distance &&
-            target_top_distance <= item1_top_distance &&
-            80 > ((item1_top_distance/continer_size)*100) ){
+        if(target_left_distance === item_left_distance &&
+            target_top_distance <= item_top_distance &&
+            80 > ((item_top_distance/continer_size)*100) ){
         
                 
-                $("#target-char").css("left",`${target_left_distance-100}px`);
                 
-                damage = 1;
-                target_hp -= 1;
+                var left_d = 0;
+                
+                if(item === 1){
+                    target_hp -= 1;
+                    left_d = 150;
+                    $("#target-char").css("left",`${target_left_distance-left_d}px`);
+                    $(".contanier").css("animation","target_damage 20ms")
+                }
+                if(item === 2){
+                    target_hp -= 3;
+                    left_d = 300;
+                    $("#target-char").css("left",`${target_left_distance-left_d}px`);
+                }
+
+                if(item === 3){
+
+                }
+                    
+                
 
                 switch(target_hp){
 
@@ -83,15 +90,28 @@ function targetDamage(target_left_distance,target_top_distance,
                     break;
 
                     case 3: $("#target-point4").css("display",`none`);
+                    $("#target-point5").css("display",`none`);
                     break;
 
                     case 2: $("#target-point3").css("display",`none`);
+                    $("#target-point4").css("display",`none`);
+                    $("#target-point2").css("display",`none`);
+                    
                     break;
 
                     case 1: $("#target-point2").css("display",`none`);
+                    $("#target-point4").css("display",`none`);
+                    $("#target-point3").css("display",`none`);
+                    $("#target-point5").css("display",`none`);
+                    
                     break;
 
-                    case 0: $("#target-point").css("display",`none`);
+                    case 0:$(`#target-point`).css("display",`none`);
+                    $("#target-point5").css("display",`none`);
+                    $("#target-point4").css("display",`none`);
+                    $("#target-point3").css("display",`none`);
+                    $("#target-point2").css("display",`none`);
+                    
                     break;
                     default: "Error"
                 }
@@ -99,9 +119,17 @@ function targetDamage(target_left_distance,target_top_distance,
                 setTimeout(() =>{
                     
                     $("#wooden-box").css('animation','none')
-                    $("#wooden-box").css("left",`${target_left_distance-100}px`);
+                    $("#wooden-box").css("left",`${target_left_distance-left_d}px`);
+
+                    $("#bar-drop").css('animation','none')
+                    $("#bar-drop").css("left",`${target_left_distance-left_d}px`)
+
+
                     item1 =0;
-                    
+                    item2 =0;
+                    item3 =0;
+                    left_d = 0;
+                    $(".contanier").css("animation","")
                     
 
                 },600)
@@ -124,6 +152,34 @@ function endTheGame(lessThan,MoreThan,kiled){
         var abyss_top_distance  = parseInt(abyss.css("top"));
         var abyss_left_distance  = parseInt(abyss.css("left"));
         var target_char_jump_ones = 0;
+        var left_d_main_char = parseInt(main_char.css("left"))
+        var left_d_target_char = parseInt($("#target-char").css("left"))
+
+        if(left_d_main_char >= left_d_target_char){
+
+            cant_jump =1;
+            main_char.css({"animation": `lose 500ms`});
+            $("#target-char").css({"animation": ``});
+            
+            abyss.css({ "left": `${abyss_left_distance}px` })
+            main_char.css({ "top": `100%` })
+            abyss.css("animation","none")
+            $("#ground").css("animation","none")
+            $(".dead").css("display","none")
+
+                
+                   
+                
+                
+                
+        
+            setTimeout(() =>{
+                 alert("You Win!!"); }, 1000
+                 )
+        
+       clearInterval(endGame)
+
+        }
         
         target_sensorForJump(parseInt(abyss_left_distance),
         target_char_jump_ones)
@@ -133,6 +189,9 @@ function endTheGame(lessThan,MoreThan,kiled){
                  abyss_left_distance > MoreThan &&
                 mainChar_top_distance === abyss_top_distance)|| kiled == 1){
                    
+                   
+
+                    
                     cant_jump =1;
                     main_char.css({"animation": `lose 500ms`});
                     $("#target-char").css({"animation": ``});
@@ -154,7 +213,9 @@ function endTheGame(lessThan,MoreThan,kiled){
                          )
                 
                clearInterval(endGame)
+            
                 }
+               var target_sensor_left_d = parseInt($("#target_sensor_toJump").css("left"));
                var continer_size = parseInt($(".contanier").css("height"));
                var main_char_after_jump = parseInt($("#main-char").css("top"));
                var main_left_distance = parseInt($("#main-char").css("left"));
@@ -168,14 +229,26 @@ function endTheGame(lessThan,MoreThan,kiled){
                var star3_top_distance = parseInt($("#star3").css("top"));
                var item1_left_distance = parseInt($("#wooden-box").css("left"));
                var item1_top_distance = parseInt($("#wooden-box").css("top"));
+               var item2_left_distance = parseInt($("#bar-drop").css("left"));
+               var item2_top_distance = parseInt($("#bar-drop").css("top"));
                
                if(item1 === 1){
-               targetDamage(target_left_distance,target_top_distance,
-                item1_left_distance,item1_top_distance,continer_size)
+               targetDamage(1,target_left_distance,target_top_distance,
+                item1_left_distance,item1_top_distance,continer_size,target_sensor_left_d)
                }
 
+               if(item2 === 1){
+                targetDamage(2,target_left_distance,target_top_distance,
+                    item2_left_distance,item2_top_distance,continer_size,target_sensor_left_d)
+                }
+
+                if(item3 === 1){
+                    targetDamage(3,target_left_distance,target_top_distance,
+                     item1_left_distance,item1_top_distance,continer_size,target_sensor_left_d)
+                    }
+
                collectStars(1,main_char_after_jump,main_left_distance
-                ,star1_left_distance,star1_top_distance)
+                ,star1_left_distance,star1_top_distance,)
 
                 collectStars(2,main_char_after_jump,main_left_distance
                     ,star2_left_distance,star2_top_distance)
@@ -208,7 +281,7 @@ function collectStars(star,main_char_after_jump,main_left_distance,
                         break;
                         case 2: $("#star2").css("display","none");
                         total_stars += 1;
-                        reUseStars("star1",10000)
+                        reUseStars("star2",10000)
                         break;
                         case 3: $("#star3").css("display","none");
                         total_stars += 1;
@@ -355,7 +428,16 @@ $("#item1").on('click', () => {
 
 $("#item2").on('click', () => {
 
-        console.log("item2")
+    if(total_stars >= 8){
+
+        item2 += 1;
+        $("#bar-drop").css({
+            'animation': 'bar-drop 3s'
+        })
+        total_stars -= 8;
+      }
+        
+    
         
 })
 
