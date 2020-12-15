5053e94fa48cfeb7a42b2d207afb3b30b1e9a64a
ASIM SAMI <eng.asimsami@gmail.com>
2020-12-15 17:54:44 +0300

    add 3 stages for landing page with 2 button next & back

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,27 +1,60 @@
-var next = 0;
+var p_stage = 0;
 var p_before = "";
-$("#next-button").on("click",()=>{
-  
+var main_p = $("#how-to-play").text();
 
-switch(next){
-   case 0:
-    pExplainHowTP("How To play:<br><br> Collect The stars to buy itemes and use it to damage the Imposter!.<br><br>",
-    'url("../img/img-ex-click-stars-itemes.jpg")')
-    break;
-    
-    case 1: 
-    pExplainHowTP("These items You can buy it by stars to damage the Imposter to catch him!!",
-    'url("../img/img-ex-item-damage.jpg")')
+
+if(p_stage === 0){
+        
+    $("#back-button").css("display","none")
 }
 
-next += 1;
+$("#next-button").on("click",()=>{
+    
+    p_stage += 1;
+    pExpHowTPStage()
+    console.log(p_stage)
+    if(p_stage === 1){
+        
+        $("#back-button").css("display","")
+    }
 })
 
 function pExplainHowTP(nextP, nextImg ){
 
-    p_before = $("#how-to-play").text()
+
     $("#how-to-play").text("")
     $("#how-to-play").append(nextP)
     $("#imges").css("background-image",nextImg)
 
 }
+
+function pExpHowTPStage(){
+    switch(p_stage){
+        case 0:
+            pExplainHowTP("How To play:<br><br>One left click on the mouse to jump. after 0.5s click again for double jump.<br><br>",
+            'url("../img/img-ex-click-jump.jpg")')
+            break;
+        case 1:
+         pExplainHowTP("How To play:<br><br>Collect The stars to buy itemes and use it to damage the Imposter!.<br><br>",
+         'url("../img/img-ex-click-stars-itemes.jpg")')
+         break;
+         
+         case 2: 
+         pExplainHowTP("How To play:<br><br>These items You can buy it by stars to damage the Imposter to catch him!!",
+         'url("../img/img-ex-item-damage.jpg")')
+         break;
+
+
+
+         default: "Error"
+     }
+     
+}
+
+$("#back-button").on("click",()=>{
+    if(p_stage !== 0){
+
+    p_stage -= 1;
+    pExpHowTPStage()
+    }
+})  
